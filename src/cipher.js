7a50b3f3b275ae9ee632a89cef9456631e481e92
dayaGuerra <dayamandalaman@gmail.com>
2018-11-20 18:57:38 -0500

    Estructura HTML, Funciones de Cipher, Eventos JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,30 @@
 window.cipher = {
-  // ... 
+function cifrado(offset, text){
+  let arr = [];
+  for (let i = 0; i < text.length; i++) {
+    if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90){
+      arr.push(String.fromCharCode((text.charCodeAt(i)-65+offset)%26+65));
+    }
+    else if (text.charCodeAt(i) < 65) {
+      arr.push(text[i])
+    }
+
+  }
+  return arr.join('');
+};
+
+function descifrado(offset, text){
+  let arr = [];
+  for (let i = 0; i < text.length; i++) {
+    if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90){
+      arr.push(String.fromCharCode((text.charCodeAt(i)-offset+65)%26+65));
+    }
+    else if (text.charCodeAt(i) < 65) {
+      arr.push(text[i])
+    }
+
+  }
+  return arr.join('');
+};
+
 };
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const caja1 = document.getElementById('caja1');
+const espacios = document.getElementById('espacios');
+const botonUno = document.getElementById('btn-cifrar');
+const botonDos = document.getElementById('btn-descifrar');
+const mostrarResultado= document.getElementById('contenido2');
+
+botonUno.addEventListener("click",function(event){
+  event.preventDefault();
+const valorCajaUno = caja1.value.toUpperCase();
+const valorEspacios = parseInt(espacios.value);
+const resultado = cifrado(valorEspacios, valorCajaUno);
+mostrarResultado.innerHTML=resultado;
+});
+
+botonDos.addEventListener("click", function(event){
+  event.preventDefault();
+const valorCajaUno = caja1.value.toUpperCase();
+const valorEspacios = parseInt(espacios.value);
+const resultado = descifrado(valorEspacios, valorCajaUno);
+mostrarResultado.innerHTML=resultado;
+});
