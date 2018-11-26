8a756960de8c7e16322d7f947a52e9e930f626cc
dayaGuerra <dayamandalaman@gmail.com>
2018-11-26 16:51:14 -0500

    minusculas codificar

--- src/cipher.js
+++ src/cipher.js
@@ -6,6 +6,10 @@ cifrado: (offset, string) => {
     if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
       arr.push(String.fromCharCode((string.charCodeAt(i)-65+offset)%26+65));
     }
+    if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122){
+      arr.push(String.fromCharCode((string.charCodeAt(i)-97+offset)%26+97));
+    }
+
     else if (string.charCodeAt(i) < 65) {
       arr.push(string[i])
     }
