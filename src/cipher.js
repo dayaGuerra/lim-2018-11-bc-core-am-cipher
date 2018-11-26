a324ae4f6eb5a696aa54ecd094b78f765f884ee2
dayaGuerra <dayamandalaman@gmail.com>
2018-11-26 16:46:36 -0500

    INDEX HTML text por string

--- src/cipher.js
+++ src/cipher.js
@@ -1,27 +1,27 @@
 window.cipher = {
 
-cifrado: (offset, text) => {
+cifrado: (offset, string) => {
   let arr = [];
-  for (let i = 0; i < text.length; i++) {
-    if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90){
-      arr.push(String.fromCharCode((text.charCodeAt(i)-65+offset)%26+65));
+  for (let i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
+      arr.push(String.fromCharCode((string.charCodeAt(i)-65+offset)%26+65));
     }
-    else if (text.charCodeAt(i) < 65) {
-      arr.push(text[i])
+    else if (string.charCodeAt(i) < 65) {
+      arr.push(string[i])
     }
 
   }
   return arr.join('');
 },
 
-descifrado: (offset, text) => {
+descifrado: (offset, string) => {
   let arr = [];
-  for (let i = 0; i < text.length; i++) {
-    if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90){
-      arr.push(String.fromCharCode((text.charCodeAt(i)-90-offset)%26+90));
+  for (let i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
+      arr.push(String.fromCharCode((string.charCodeAt(i)-90-offset)%26+90));
     }
-    else if (text.charCodeAt(i) < 65) {
-      arr.push(text[i])
+    else if (string.charCodeAt(i) < 65) {
+      arr.push(string[i])
     }
 
   }
