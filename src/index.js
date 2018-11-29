d704c6962feb1bfd06db2c6ce7ea64a4449d8a94
dayaGuerra <dayamandalaman@gmail.com>
2018-11-28 22:32:08 -0500

    CSS Ventana modal responsive - igualar cuadros de texto

--- src/index.js
+++ src/index.js
@@ -22,7 +22,9 @@ const valueOffset = parseInt(offset.value);
 const result = cipher.encode(valueOffset, valueInputText);
 outputText.innerHTML= result;
 
+placeHolderOutput.style.display='none';
 reference.innerHTML='El codigo para Descifrar es: ' + valueOffset;
+
 });
 
 btnDecode.addEventListener('click',function(event){
@@ -32,6 +34,7 @@ const valueOffset = parseInt(offset.value);
 const result = cipher.decode(valueOffset, valueInputText);
 outputText.innerHTML= result;
 
+placeHolderOutput.style.display='none';
 reference.innerHTML='El codigo para Cifrado fue: ' + valueOffset;
 })
 
