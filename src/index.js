956336b2fb22976163e0ef720f933c0e3cbaec73
dayaGuerra <dayamandalaman@gmail.com>
2018-12-03 13:31:41 -0500

    CSS - Borrar opacidad, INDEXHTML - cambiar p por li, mover boton de limpiar a form,  INDEXJS - funcion reset()

--- src/index.js
+++ src/index.js
@@ -3,34 +3,34 @@ const offset = document.getElementById('offset');
 const btnEncode = document.getElementById('btn-encode');
 const btnDecode = document.getElementById('btn-decode');
 const outputText = document.getElementById('outputText');
-const btnRefresh = document.getElementById('btn-refresh');
+const btnCleanForm = document.getElementById('btn_clean_form');
 const btnCopy = document.getElementById('btn-copy');
+const formCipher = document.getElementById('form_cipher');
 
 btnEncode.addEventListener('click',function(event){
-   event.preventDefault();
-    const valueInputText = inputText.value;
-    const valueOffset = parseInt(offset.value);
-    const result = cipher.encode(valueOffset, valueInputText);
-    const reference='El codigo para Descifrar es: ' + valueOffset;
-   
-  outputText.innerHTML=  ` ${result}
-  ${reference}`;
+event.preventDefault();
+const valueInputText = inputText.value;
+const valueOffset = parseInt(offset.value);
+const result = cipher.encode(valueOffset, valueInputText);
+outputText.innerHTML=  result;
 });
 
 btnDecode.addEventListener('click',function(event){
- event.preventDefault();
-  const valueInputText = inputText.value;
-  const valueOffset = parseInt(offset.value);
-  const result = cipher.decode(valueOffset, valueInputText);
+event.preventDefault();
+const valueInputText = inputText.value;
+const valueOffset = parseInt(offset.value);
+const result = cipher.decode(valueOffset, valueInputText);
 outputText.innerHTML= result;
 })
 
-btnRefresh.addEventListener('click',function(event){
-event.preventDefault();
-  window.location.href = 'index.html'
+btnCleanForm.addEventListener('click', ()=>{
+  formCipher.reset();
+  outputText.innerHTML = "";
+ 
 });
 
 btnCopy.addEventListener('click', ()=>{
-  outputText.select();
-  document.execCommand('copy'); 
+outputText.select();
+document.execCommand('copy');
+
 });
